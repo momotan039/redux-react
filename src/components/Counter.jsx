6cedd14d2f5d058fc6eb0075602336f29360e4a9
momotan <momotaha039@gmail.com>
2023-04-01 03:01:18 +0300

    add slice using toolkit adding counterSlice that contain actions and types

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  decrementAction,
-  fetchCounterAction,
-  incrementAction,
-} from "../redux/actions/counterActions";
+import { decrement, increment } from "../redux/counterSlice";
 
 function Counter() {
-  const { count } = useSelector((state) => state);
+  const { count } = useSelector((state) => state.counter);
   const dispatch = useDispatch();
-  const handelDecrement = () => dispatch(incrementAction);
-  const handelIncrement = () => dispatch(decrementAction);
-  const handelFetchCounter = (e) => fetchCounterAction(dispatch,e.target);
+  const handelDecrement = () => dispatch(decrement());
+  const handelIncrement = () => dispatch(increment());
   return (
     <>
       <h1>Count:{count}</h1>
       <button onClick={handelIncrement}>plus 1</button>
       <button onClick={handelDecrement}>minus 1</button>
-      <button onClick={handelFetchCounter}>fetch counter from api for 3 seconds</button>
     </>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 import counterReducer from "./reducers/counterReducer";
 import reduxThunk from "redux-thunk";
-const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counterSlice";
+// const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-const store = createStore(
-  counterReducer,
-  enhancer(applyMiddleware(reduxThunk))
-);
+// const store = createStore(
+//   counterReducer,
+//   enhancer(applyMiddleware(reduxThunk))
+// );
 
+const store=configureStore({
+    reducer:{
+        counter:counterSlice
+    }
+})
 export default store;
